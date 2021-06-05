19c04c0e9791289467231590f49436e99bda3b35
Hamza <hamzaable@gmail.com>
2021-06-05 02:27:51 +0200

    Updates

---
+++ precache-manifest.b40a5446f259a87e9d3172a39a29493b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7484ea14a235c845cf6ac38888c8819a",
+    "url": "/ChatsBoard/index.html"
+  },
+  {
+    "revision": "cae924c1bdfbfd8ea014",
+    "url": "/ChatsBoard/static/css/main.a8d60d7c.chunk.css"
+  },
+  {
+    "revision": "a1ea03cfd3cdaf52ab27",
+    "url": "/ChatsBoard/static/js/2.af5cf525.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ChatsBoard/static/js/2.af5cf525.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cae924c1bdfbfd8ea014",
+    "url": "/ChatsBoard/static/js/main.f89e34fb.chunk.js"
+  },
+  {
+    "revision": "1eadb13b53f72e0f7d6a",
+    "url": "/ChatsBoard/static/js/runtime-main.42d7a223.js"
+  }
+]);
